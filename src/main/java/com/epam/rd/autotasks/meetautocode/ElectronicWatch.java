911c9cd4f9c5a6c3cd14cf0ce7d9f688941d8b3a
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-05-27 21:17:25 +0300

    Answer for fifth task

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -9,6 +9,6 @@ public class ElectronicWatch {
         int hh = (seconds/3600)-(24*((seconds/3600)/24));
         int mm = (seconds - ((seconds/3600)*3600))/60;
         int ss = seconds - (((seconds/3600)*3600) + (mm*60));
-        System.out.println(hh + ":" + mm + ":" + ss);
+        System.out.println(hh + ":" + ((mm < 10) & (ss < 10)? "0" + mm + ":" + "0" + ss : (mm > 9) & (ss > 9)? mm + ":" + ss : (mm > 9) & (ss < 10)? mm + ":" + "0" + ss : "0" + mm + ":"  + ss));
     }
 }
