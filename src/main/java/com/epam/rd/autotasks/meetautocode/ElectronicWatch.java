c697eb36a4c822694d9eb39ef7cd4c19de848ccb
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-05-27 20:36:34 +0300

    Answer for fifth task

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,9 +6,9 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-        int hh = seconds/3600;
-        int mm = (seconds - (hh*3600))/60;
-        int ss = seconds - ((hh*3600) + (mm*60));
+        int hh = (seconds/3600)-(24*((seconds/3600)/24));
+        int mm = (seconds - ((seconds/3600)*3600))/60;
+        int ss = seconds - (((seconds/3600)*3600) + (mm*60));
         System.out.println(hh + ":" + mm + ":" + ss);
     }
 }
